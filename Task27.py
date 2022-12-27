b66b6fe21d6363d6e7457e9a400e143df8dc1a91
Evgeny Fateev <fateyev_84@mail.ru>
2022-12-27 15:40:44 +0500

    A24 Task27 complete

--- Task27.py
+++ Task27.py
@@ -2,3 +2,17 @@
 # Пример:
 # 4, 5 -> 20
 # 4,6 -> 12
+x = int(input('Введите число x: '))
+y = int(input('Введите число y: '))
+def calculate_lcm(x, y):
+    if x > y:
+        greater = x
+    else:
+        greater = y
+    while(True):
+        if((greater % x == 0) and(greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
+    return lcm
+print(f'{x}, {y} -> {calculate_lcm(x, y)}')
