5dc8bf9a53cda9f92b8cd8cc3a8a8c6beafbcc7a
Evgeny Fateev <fateyev_84@mail.ru>
2023-01-07 21:05:18 +0500

    A41 task12 fixed

--- Task12.py
+++ Task12.py
@@ -1,10 +1,11 @@
 # Программа, которая принимает на вход число N и выдаёт последовательность из N членов.
 # - Для N = 5: 1, -3, 9, -27, 81
+
 # Первое решение
 N = int(input('Введите число: '))
 s = 0
 while s < N:
-    print(f'{(-3) ** s}', end=' ')
+    print(f'{(-3) ** s}', end=', ')
     s += 1
 
 print()  # /------
@@ -17,8 +18,12 @@ for i in range(n):
 print()  # /------
 
 # Третье решение
-n = int(input('Введите число: '))
-a = []
-for i in range(n):
-    a.append((-3) ** i)
-print(a)
+
+def row_powers3():
+    n = int(input('Введите число: '))
+    a = []
+    for i in range(n):
+        a.append((-3) ** i)
+    print(a)
+
+row_powers3()
