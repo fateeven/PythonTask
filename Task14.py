910b80db411852b889740631d1a89a5cefb1ca8f
Evgeny Fateev <fateyev_84@mail.ru>
2023-01-07 21:46:47 +0500

    A46 task14 fixed

--- Task14.py
+++ Task14.py
@@ -4,11 +4,30 @@
 # абвгдабвгд -> 2
 # абв
 
-a = input('Ввведите текст: ')
-b = input('Введите искомый элемент: ')
-if len(a) > len(b):
-    print(f'В тексте: {a} -> Элемент: {b} -> Найден: {a.count(b)} раз')
-elif a == b:
-    print(f'В тексте: {a} -> Элемент: {b} -> Найден: 1 раз')
-else:
-    print(f'Ошибка ввода')
+# a = input('Ввведите текст: ')
+# b = input('Введите искомый элемент: ')
+# if len(a) > len(b):
+#     print(f'В тексте: {a} -> Элемент: {b} -> Найден: {a.count(b)} раз')
+# elif a == b:
+#     print(f'В тексте: {a} -> Элемент: {b} -> Найден: 1 раз')
+# else:
+#     print(f'Ошибка ввода')
+
+
+def count_str(text_str, element_str):
+    """определяет количество вхождений одной строки в другой"""
+    if len(text_str) > len(element_str):
+        print(f'В тексте: {text_str} \nЭлемент: {element_str} '
+                  f'-> Найден: {text_str.count(element_str)} раз')
+    elif text_str == element_str:
+        print(f'В тексте: {text_str} \nЭлемент: {element_str} '
+              f'-> Найден: 1 раз')
+    else:
+        print(f'Ошибка ввода')
+
+    return text_str.count(element_str)
+
+
+text = input('Ввведите текст: ')
+element = input('Введите искомый элемент: ')
+count_str(text, element)
