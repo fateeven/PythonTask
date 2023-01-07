73e300ee3869eaaf1c2ee9ac2a31a58b2148aa52
Evgeny Fateev <fateyev_84@mail.ru>
2023-01-07 21:30:27 +0500

    A48 task16 fixed

--- Task16.py
+++ Task16.py
@@ -8,3 +8,20 @@ num = int(input('Введите число: '))
 result = [round((1 + 1 / i) ** i, 2) for i in range(1, num + 1)]
 print(f'Список чисел: {result} ')
 print(f'Сумма чисел в списке: {sum(result)} ')
+
+
+def list_of_numbers(number):
+    """Задает список из number чисел последовательности-(1 + 1/n)**n"""
+    n = 1
+    list = []
+    while n < number + 1:
+        result = (1 + 1 / n) ** n
+        n += 1
+        list.append(round(result, 2))
+
+    print(f'Список чисел: {list} ')
+    print(f'Сумма чисел в списке: {sum(list)} ')
+
+
+nums = int(input(f'Введите число: '))
+list_of_numbers(nums)
